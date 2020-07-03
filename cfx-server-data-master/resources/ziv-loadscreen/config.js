22af961f7e7523633c7f17f704b25e28e8fa8337
Stef_Fokianos <stfokianos@gmail.com>
2020-07-03 07:24:56 +0300

    +Added custom loading screen +Updated AllResources File

---
+++ cfx-server-data-master/resources/ziv-loadscreen/config.js
@@ -0,0 +1,11 @@
+//-----------------Config-----------------
+var title = "Ricazel RP is OP";
+var link = "Enjoy!";
+//---------------End Config---------------
+
+
+document.getElementById('title').innerHTML = title;
+document.getElementById('link').innerHTML = link;
+ 
+
+console.log('Connected.')

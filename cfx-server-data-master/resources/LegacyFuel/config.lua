70032c279cdae3cced83282a4bc63eb0760c47f7
Stef_Fokianos <stfokianos@gmail.com>
2020-07-05 09:13:24 +0300

    +Fixed esx_skin
    +Added character customazation
    +Added menu lists
    +Fixed inventory menu
    +Fixed character customaztion
    +Minor changes
    
    >Changed noclip to F11
    >F2 is now used for inventory
    >F3 is now used for emotes
    >F1 is now used for phone
    >F7 is now used for bills

--- cfx-server-data-master/resources/LegacyFuel/config.lua
+++ cfx-server-data-master/resources/LegacyFuel/config.lua
@@ -14,11 +14,11 @@ Config.FuelDecor = "_FUEL_LEVEL"
 Config.DisableKeys = {0, 22, 23, 24, 29, 30, 31, 37, 44, 56, 82, 140, 166, 167, 168, 170, 288, 289, 311, 323}
 
 -- Want to use the HUD? Turn this to true.
-Config.EnableHUD = true
+Config.EnableHUD = false
 
 -- Configure blips here. Turn both to false to disable blips all together.
-Config.ShowNearestGasStationOnly = true
-Config.ShowAllGasStations = false
+Config.ShowNearestGasStationOnly = false
+Config.ShowAllGasStations = true
 
 -- Modify the fuel-cost here, using a multiplier value. Setting the value to 2.0 would cause a doubled increase.
 Config.CostMultiplier = 1.0
